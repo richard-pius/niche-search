362a3c33085efac8d69326692995a36f78076d87
Richard Pius <richardpius2005@gmail.com>
2026-05-28 00:15:33 +0530

    Revert to private ACL – use bucket policy for public access

--- niche_search/settings.py
+++ niche_search/settings.py
@@ -100,7 +100,7 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', None)
 AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME', 'your-bucket')
 AWS_S3_REGION_NAME = os.environ.get('AWS_REGION', 'us-east-1')
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-AWS_DEFAULT_ACL = 'public-read'
+AWS_DEFAULT_ACL = 'private'
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
